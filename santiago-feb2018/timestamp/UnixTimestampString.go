532a541adcfbfdb9025b5b94abcd3b05d550ef60
Freddy Rondon <ifreddyrondon@gmail.com>
2018-02-19 18:34:58 -0300

    santiago-feb2018: add example of unmarshal timestamp as string

---
+++ santiago-feb2018/timestamp/UnixTimestampString.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/ifreddyrondon/go-talks/santiago-feb2018/timestamp/timeutils"
+)
+
+func main() {
+	// START OMIT
+	in := []byte(`{"date": "630655260"}`)
+	// END OMIT
+	var t timeutils.Timestamp
+	if err := json.Unmarshal(in, &t); err != nil { // HL
+		panic(err)
+	}
+	fmt.Println(t)
+}
