17cabc2cd52b5431f9ce8cac13bb919e31733117
Freddy Rondon <ifreddyrondon@gmail.com>
2018-02-19 22:50:27 -0300

    santiago-feb2018: All that glisters is not gold

---
+++ santiago-feb2018/captures/capture/unmarshal.go
@@ -0,0 +1,62 @@
+package capture
+
+import (
+	"encoding/json"
+	"sort"
+	"sync"
+)
+
+type indexCapture struct {
+	index int
+	*Capture
+}
+
+type job struct {
+	index int
+	data  json.RawMessage
+}
+
+func worker(wg *sync.WaitGroup, jobs <-chan job, results chan<- indexCapture) {
+	for job := range jobs {
+		var c Capture
+		if err := json.Unmarshal(job.data, &c); err == nil {
+			results <- indexCapture{index: job.index, Capture: &c}
+		}
+		wg.Done()
+	}
+}
+
+func (p *Captures) UnmarshalJSON(data []byte) error {
+	var raw []json.RawMessage
+	if err := json.Unmarshal(data, &raw); err != nil {
+		panic(err)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(raw))
+	jobs := make(chan job, len(raw))
+	results := make(chan indexCapture, len(raw))
+
+	for w := 0; w < WorkersNumber; w++ {
+		go worker(&wg, jobs, results)
+	}
+	for i, v := range raw {
+		jobs <- job{index: i, data: v}
+	}
+	close(jobs)
+	wg.Wait()
+	close(results)
+
+	processed := make([]indexCapture, 0, len(results))
+	for data := range results {
+		processed = append(processed, data)
+	}
+	sort.Slice(processed, func(i, j int) bool {
+		return processed[i].index < processed[j].index
+	})
+	for _, v := range processed {
+		*p = append(*p, *v.Capture)
+	}
+
+	return nil
+}
